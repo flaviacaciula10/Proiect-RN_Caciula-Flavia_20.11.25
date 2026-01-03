8d44470ff377d30f29cb7f2e0fd99906d173b355
flaviacaciula10 <caciulaflavia10@gmail.com>
2026-01-03 19:07:08 +0200

    Delete Recunoasterea varstei/config directory

--- Recunoasterea varstei/config/config.py
+++
@@ -1,5 +0,0 @@
-INPUT_DIR = r'E:\rn proiect\Recunoasterea varstei\data\raw'
-OUTPUT_DIR = 'date_procesate'
-BACKGROUND_REMOVAL_DIR = 'date_fara_fundal'
-TARGET_SIZE = (150, 150)
-GRAYSCALE_ENABLED = True
